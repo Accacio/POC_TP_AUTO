3e3865fda236e53a7dfdaf42f436b17ec69aea4a
Accacio <raccacio@poli.ufrj.br>
2022-06-08 14:58:14 +0200

    optional file with simple documentation

---
+++ Example/signImage.m
@@ -0,0 +1,13 @@
+%% signImage - sign an image to be used 
+%% in reports for the «TP Automatique»
+%% usage signImage("imagepath.ext")
+%%
+%% A «imagepath_signed.png» image is created. 
+%% 
+%% PDFs created using word should export 
+%% images losslessly
+%% 
+%% PDFs created using latex do not need 
+%% special attention
+
+%% Get PC metadata 
